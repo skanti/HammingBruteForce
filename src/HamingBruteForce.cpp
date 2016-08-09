1a18fe7297e89b80637976410eab40f93d074c27
Armen Avetisyan <armen.avetisyan.to@gmail.com>
2016-08-09 20:46:03 +0900

    Implementing 64 bit vectorized haming brute force matching.

--- src/HamingBruteForce.cpp
+++ src/HamingBruteForce.cpp
@@ -1,5 +1,4 @@
 #include "HamingBruteForce.h"
-
 template<typename T>
 HamingBruteForce<T>::HamingBruteForce(int n_size_reserve_, int n_dim_, int threshold_)
         : n_size_reserve(n_size_reserve_), n_dim(n_dim_), threshold(threshold_), index_ab(n_size_reserve, -1),
@@ -16,7 +15,8 @@ void HamingBruteForce<T>::match_all(const T *a, int n_size_a, const T *b, int n_
         for (int j = 0; j < n_size_b; j++) {
             int d_ab = 0;
             for (int k = 0; k < ld; k++)
-                d_ab += __builtin_popcount(a[i * ld + k] ^ b[j * ld + k]);
+                d_ab += __builtin_popcountl(a[i * ld + k] ^ b[j * ld + k]);
+            //std::cout << std::bitset<64>(d_ab)
             int is_closer = d_ab < distance_ab[i];
             index_ab[i] = is_closer ? j : index_ab[i];
             distance_ab[i] = is_closer ? d_ab : distance_ab[i];
